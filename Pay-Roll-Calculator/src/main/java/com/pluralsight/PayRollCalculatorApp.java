1732266d5c3af6b951407583a08e84d758b22fc8
Eyob Mengistu <emengistu17@gmail.com>
2025-04-23 19:05:26 -0500

    Completed Exercise Payroll Calculator with buffer writer

--- Pay-Roll-Calculator/src/main/java/com/pluralsight/PayRollCalculatorApp.java
+++ Pay-Roll-Calculator/src/main/java/com/pluralsight/PayRollCalculatorApp.java
@@ -1,40 +1,70 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class PayRollCalculatorApp {
-        static Scanner userPrompt = new Scanner(System.in);
-    public static void main(String[] args) {
-        try{
+    
+    // ================ Fire up the scanner =================
+    static Scanner userPrompt = new Scanner(System.in);
 
-            // create a FileReader object connected to the File
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+    public static void main(String[] args) {
+
+        // ================= ASk the user for input and output file names =================
+        String inputFile = question("Enter the name of the input file\n");
+        String outputFile = question("Enter the name of the out put file \n");
+
+        try {
+            // ================= Create a FileReader object connected to the File =================
+            FileReader fileReader = new FileReader("src/main/resources/" + inputFile);
             // create a BufferedReader to manage input stream
             BufferedReader bufReader = new BufferedReader(fileReader);
 
-            String token;
-            while((token = bufReader.readLine()) != null) {
+            // ================= Create a FileWriter =================
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + outputFile);
 
-                String[] inputParts = token.split("\\|");
-                if(inputParts[0].equals("id")){
+            // ================= Create a BufferedWriter =================
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            // Create a header row on the csv file which is  a string
+            bufWriter.write("id" + "|" + "name" + "|" + "gross pay" + "\n");
+
+
+            // ================= Create a variable to hold the input =================
+            String theLine;
+            while ((theLine = bufReader.readLine()) != null) {
+
+                String[] inputParts = theLine.split("\\|");
+
+                // ================= Check to see if the first part of the line is equal to id =================
+                if (inputParts[0].equals("id")) {
                     continue;
                 }
-                // Create a variable to hold the parsed values
+                // ================= Create a variable to hold the parsed values =================
                 int id = Integer.parseInt(inputParts[0]);
-                String name= inputParts[1];
-                double hours= Double.parseDouble(inputParts[2]);
-                double rate=Double.parseDouble(inputParts[3]);
+                String name = inputParts[1];
+                double hours = Double.parseDouble(inputParts[2]);
+                double rate = Double.parseDouble(inputParts[3]);
 
-                System.out.printf("%d - %s - $%.2f\n", id, name, Employee.getGrossPay(hours,rate));
+                bufWriter.write(id + "|" + name + "|" + Employee.getGrossPay(hours, rate) + "\n");
+
 
             }
-
+            // ================= Close the buffer ================
             bufReader.close();
+            bufWriter.close();
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    public static String question(String message) {
+        System.out.print(message);
+        return userPrompt.nextLine();
+    }
 }
+
